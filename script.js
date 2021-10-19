0644c818720319fc68bad9e9fed4573fdf38532b
Krister Viirsaar <git@krister.ee>
2021-10-19 10:07:30 +0000

    Download chat messages and log how many are there

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 console.log("Program starting")
+const SDB_URL = "https://tinkr.tech/sdb/chatapp"
 
 async function downloadMessages() {
     console.log("Downloading messages from server")
+    const messages = await fetchGET(SDB_URL)
+    console.log('Downloaded message count:', messages.length)
 }
 
 downloadMessages()
