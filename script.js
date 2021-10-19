1aea099a1eabfcdc92b277329be73a43a3e0f56d
Krister Viirsaar <git@krister.ee>
2021-10-19 12:14:48 +0000

    Cleanup: remove all console.log() lines

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-console.log("Program starting")
 const SDB_URL = "https://tinkr.tech/sdb/chatapp"
 const messagesContainer = document.querySelector('#messages-container')
 
 async function downloadMessages() {
-    console.log("Downloading messages from server")
     const messages = await fetchGET(SDB_URL)
-    console.log('Downloaded message count:', messages.length)
-    console.log('First message content:', messages[0])
-    console.log('First message user:', messages[0].user)
 
     messagesContainer.innerHTML = null
     for (const message of messages) {
@@ -15,7 +10,6 @@ async function downloadMessages() {
         messageHTML += '<p class="user">' + message.user + '</p>'
         messageHTML += '<p class="content">' + message.message + '</p>'
         messageHTML += '</div>'
-        console.log('messageHTML', messageHTML)
         messagesContainer.innerHTML += messageHTML
     }
 }
@@ -23,16 +17,12 @@ async function downloadMessages() {
 downloadMessages()
 
 const messageInput = document.querySelector('input#message')
-console.log("Selected element with id:", messageInput.id)
 
 messageInput.onkeyup = async function (event) {
-    console.log('User is typing:', event.target.value)
     const message = event.target.value
 
     if (event.key === 'Enter') {
-        console.log('Enter pressed, send message')
         const userInput = document.querySelector('input#user')
-        console.log('userInput.value', userInput.value)
         const data = { user: userInput.value, message: message }
         await fetchPOST(SDB_URL, data)
         messageInput.value = null
